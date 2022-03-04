18eb0d1f390f887007e0983c90caec8f43e425f2
kotov_aa <kotov.aa.logist@gmail.com>
2022-03-04 20:29:11 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Введите целое число от 1 до 12 : ");
+        int value = new Scanner(System.in).nextInt();
+        int factorial = 1;
+
+        System.out.print(value + "! = 1");
+        for (int i = 2; i <= value; i = i + 1) {
+            System.out.print(" x " + i);
+            factorial = factorial * i;
+        }
+        System.out.print(" = " + factorial);
+    }
+}
